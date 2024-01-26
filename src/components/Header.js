816cebd20e4844df3962de97fb21af6ac78a054d
Juan <juanpo.alvarado@gmail.com>
2024-01-26 15:58:36 -0600

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Div = styled.div`
 
 const Header = (props) => {
   const [i, setI] = useState(0);
-  const text1 = "Juan Pablo Alvarado\nCloud Engineer";
+  const text1 = "Juan Pablo Alvarado\nBachelor in Computer Engineering";
 
   useEffect(() => {
     setTimeout(
